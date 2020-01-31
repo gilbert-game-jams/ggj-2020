a3a11c2899ad1c439cf11bda416389e9c53e9631
GilbertNordhammar <gilbertnordhammar@gmail.com>
2020-01-31 19:55:51 +0100

    created basic player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 3.5f;
+     private float X;
+     private float Y;
+    
+    void Start ()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+ 
+     void Update() {
+        Debug.Log(Input.GetAxis("Mouse Y") * speed);
+        transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y") * speed, Input.GetAxis("Mouse X") * speed, 0));
+        X = transform.rotation.eulerAngles.x;
+        Y = transform.rotation.eulerAngles.y;
+        transform.rotation = Quaternion.Euler(X, Y, 0);
+     }
+}
