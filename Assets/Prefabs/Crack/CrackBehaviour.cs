3fe5dcb8991b60add1d28be23694fbc9fed52b87
GilbertNordhammar <gilbertnordhammar@gmail.com>
2020-02-01 11:48:54 +0100

    added cracks with behaviour

---
+++ Assets/Prefabs/Crack/CrackBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrackBehaviour : MonoBehaviour
+{
+    public enum CrackState { Repaired, Broken }
+
+    public GameObject _fixedCrack;
+    public GameObject _brokenCrack;
+    
+    [Range(0f, 30f)]
+    public float _timeUntilDespawn = 5.0f;
+
+    CrackState _crackState = CrackState.Broken;
+    float _timeSinceFixed = 0.0f;
+    private void Awake() {
+        SetCrackState(CrackState.Broken);
+    }
+
+
+    void SetCrackState(CrackState state) {
+        _crackState = state;
+        switch(state) {
+            case CrackState.Broken:
+                _fixedCrack.SetActive(false);
+                _brokenCrack.SetActive(true);
+                break;
+            case CrackState.Repaired:
+                _fixedCrack.SetActive(true);
+                _brokenCrack.SetActive(false);
+                break;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other) 
+    {
+        if (other.gameObject.GetComponent<ArrowBehaviour>()) 
+        {
+            SetCrackState(CrackState.Repaired);
+        }
+    }
+
+    private void Update() 
+    {
+        if(_crackState == CrackState.Repaired) {
+            _timeSinceFixed += Time.deltaTime;
+            if(_timeSinceFixed > _timeUntilDespawn) {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
