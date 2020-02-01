540a588eedd46ddf84ec54b2f34aa00807fcd232
GilbertNordhammar <gilbertnordhammar@gmail.com>
2020-02-01 14:00:02 +0100

    Updated how arrow speed works

--- Assets/Prefabs/Bow/BowBehaviour.cs
+++ Assets/Prefabs/Bow/BowBehaviour.cs
@@ -9,7 +9,11 @@ public class BowBehaviour : MonoBehaviour
     public GameObject _arrowSpawn;
     public Slider _slider;
     
-    [Range(1, 20f)]
+    
+    [Range(1, 50f)]
+    public float _minimumArrowSpeed = 5f;
+
+    [Range(1, 50f)]
     public float _maximumArrowSpeed = 10f;
     
     [Range(0.1f, 10f)]
@@ -18,10 +22,23 @@ public class BowBehaviour : MonoBehaviour
     float _drawTime = 0.0f;
     float _drawFactor = 0.0f;
 
+    private void OnValidate() 
+    {
+        _maximumArrowSpeed = _minimumArrowSpeed > _maximumArrowSpeed ? _minimumArrowSpeed : _maximumArrowSpeed;
+    }
+
+    float GetArrowSpeed(float minSpeed, float maxSpeed, float draw) {
+        draw = Mathf.Clamp(draw, 0, 1);
+        return Mathf.Lerp(minSpeed, maxSpeed, draw);
+    }
+
     void FireArrow(float drawFactor, float maxSpeed)
     {
         var arrow = GameObject.Instantiate(_arrow, _arrowSpawn.transform.position, _arrow.transform.rotation);
-        arrow.GetComponent<Rigidbody>().velocity = -transform.forward * maxSpeed * drawFactor;
+
+        // arrow.GetComponent<Rigidbody>().velocity = -transform.forward * maxSpeed * drawFactor;
+        arrow.GetComponent<Rigidbody>().velocity = -transform.forward * GetArrowSpeed(_minimumArrowSpeed, _maximumArrowSpeed, drawFactor);
+        
         var arrowSpawnTrans = _arrowSpawn.transform;
         arrow.transform.LookAt(arrowSpawnTrans.position + arrowSpawnTrans.forward, arrowSpawnTrans.up);
     }
