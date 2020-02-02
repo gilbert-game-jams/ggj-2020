6f05f934ceb39ff1d02e7962b91116b0c3fc07cc
Christoffer Tornell <christoffer.tornell@gmail.com>
2020-02-02 13:08:20 +0100

    Added a water flow sound for the dam

---
+++ Assets/dammWaterFlowSound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dammWaterFlowSound : MonoBehaviour
+{
+    [FMODUnity.EventRef] public string waterFlowEvent;
+    private FMOD.Studio.EventInstance waterFlowInstance;
+
+    private void Start()
+    {
+        waterFlowInstance = FMODUnity.RuntimeManager.CreateInstance(waterFlowEvent);
+        waterFlowInstance.start();
+    }
+
+    private void OnDestroy()
+    {
+        waterFlowInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+    }
+}
